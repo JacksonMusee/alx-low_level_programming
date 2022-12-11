cfd69394855d8006ed69dd32f5e00ac7dd3ad81e
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-11 10:36:28 -0800

    Printf()

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*
+* main - Entry point bro
+*
+* Return: zero for success
+*
+*/
+
+int  main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art," );
+
+	return (0);
+}
