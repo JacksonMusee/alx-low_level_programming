15a8c341bb1b1faf582dafe75c68df4c4a464eba
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-13 12:20:53 -0800

    Check last digt

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,17 +18,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_dig = n%10;
+	last_dig = n % 10;
 
 	if (last_dig > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n,last_dig);
-	} else if (n == 0)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_dig);
+	} else if (last_dig == 0)
 	{
-		printf("Last digit of %d is %d and is zero\n", n,last_dig);
-	} else if (n < 6 && n != 0)
+		printf("Last digit of %d is %d and is zero\n", n, last_dig);
+	} else if (last_dig < 6 && last_dig != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,last_dig);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_dig);
 	}
 
 	return (0);
