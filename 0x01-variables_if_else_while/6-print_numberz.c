cdb141e6b2a5432d5b05a8fc899b2a9f5b8004af
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-13 13:39:06 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: Checking if number is negative
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0);
+}
