24cfcc9102c90128166980bc59403ebf928f6233
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-13 12:14:14 -0800

    Check last digt

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: Checking if number is negative
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int last_dig = n%10;
+
+	if (last_dig > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n,last_dig);
+	} else if (n == 0)
+	{
+		printf("Last digit of %d is %d and is zero\n", n,last_dig);
+	} else if (n < 6 && n != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,last_dig);
+	}
+
+	return (0);
+}
