8d5de8fb84db7a256a8eb416be9ecf43f938d27d
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-13 15:33:39 -0800

    Wueh

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: Checking if number is negative
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ifirst;
+	int i;
+	int jfirst;
+	int j;
+
+	for (ifirst = 48; ifirst < 58; ifirst++)
+	{
+		for (i = 48; i < 58; I++)
+		{
+			j = i + 1;
+			jfirst = ifirst;
+			for (;jfirst < 58; jfirst++)
+			{
+				for (; j < 58; j++)
+				{
+					putchar(ifirst);
+					putchar(i);
+					putchar(' ');
+					putchar(jfirst);
+					putchar(j);
+					if (ifirst != 57 || jfirst !== 57 || i != 56 || j != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				j = 48;
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
