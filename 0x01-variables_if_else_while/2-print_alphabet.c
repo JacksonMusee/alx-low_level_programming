5b412539cbfb5ae7bcf537933c985af7c145970c
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-13 12:48:04 -0800

    Print aphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: Checking if number is negative
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int letter;
+
+	for(letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	return (0);
+}
