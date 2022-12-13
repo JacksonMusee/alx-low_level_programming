9ba370ab5955f8f6f01e21ece7a2b40e9d681972
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-13 14:01:16 -0800

    Resilience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: Checking if number is negative
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
