42ac4a363900539050bd1fd6f791792cc66ae4c6
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-15 13:02:54 -0800

    Auto

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: Obvious
+ *
+ * Return: Always 0 (Success)
+ */
+
+void main(void)
+{
+	printf("_putchar\n");
+
+	return(0);
+}
