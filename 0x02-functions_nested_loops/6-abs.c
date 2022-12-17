1a80aae8dde242a89063e32cd7e10ddf18da60df
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-17 05:49:15 -0800

    Do absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - Do absolute value
+ *
+ * @c: Number to check
+ *
+ * Return: gives absolute value
+ */
+
+int _abs(int c)
+{
+	int abs_val;
+
+	if ( c > 0 )
+	{
+	abs_val = c;
+	}
+	else
+	{
+	abs_val = c * -1;
+	}
+	return (abs_val);
+}
