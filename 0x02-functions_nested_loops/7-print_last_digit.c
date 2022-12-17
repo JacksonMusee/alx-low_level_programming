7a0f92a34d0a862dad2f7298060f1f5224a745be
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-17 06:53:39 -0800

    Get last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_last_digit - print last digit
+ *
+ *@num: number to be checked
+ *
+ * Return: return the last digit
+ */
+
+int print_last_digit(int num)
+{
+	last_dig = num % 10;
+	_putchar(last_dig);
+	return (last_dig);
+}
