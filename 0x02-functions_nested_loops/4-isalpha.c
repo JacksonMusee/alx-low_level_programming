c86c40928427a599b962f14bb73d9c7d1fc5c0a1
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-17 04:28:02 -0800

    Check if alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * _isalpha - check if it's alphabet
+ *
+ * @c : The letter to be checked eg 'H'
+ *
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
