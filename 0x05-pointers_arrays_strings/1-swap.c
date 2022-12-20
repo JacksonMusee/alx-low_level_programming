4de6991200f91cc850353e898021c6736a38b5b8
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-20 11:46:37 -0800

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *swap_int - See code
+ *
+ *Return: void
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int orig_a;
+
+	orig_a = *a;
+	*a = *b;
+	*b = orig_a;
+
+	return
+}
