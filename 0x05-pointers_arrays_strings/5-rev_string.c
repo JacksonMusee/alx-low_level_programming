9ec89d94f1e3c380752c89758b80013dfa817b38
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-20 14:55:33 -0800

    Reverser

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *rev_string - Function
+ *
+ *@s: string
+ *
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	char rd_s[10];
+	int j;
+
+	while (s[i] != '\0')
+	{
+	i++
+	}
+	i = i-1;
+	j = 0;
+
+	while (i >= 0)
+	{
+	rd_s[j] = s[i];
+	i--;
+	j++;
+	}
+	s = rd_s;
+}
