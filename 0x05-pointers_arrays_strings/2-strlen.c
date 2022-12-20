d90fbc4f7c143d36daf291dacd0810d9969c9d0a
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-20 12:20:20 -0800

    Length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+int _strlen(char *s)
+{
+	
+	int i;
+
+	i = 0;
+
+	while(s[i] != NULL)
+	{
+	strlen = i+1;
+	i++;
+	}
+
+	return (strlen);
+}
