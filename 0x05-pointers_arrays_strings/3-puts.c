f12f2ae5bd8cabf8bc29b8be75a90c0539ebf318
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-20 13:26:04 -0800

    Auto

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_puts - print
+ *
+ *str: string to print
+ *
+ *
+ */
+
+void _puts(char *str)
+{
+	
+	while (*str != '\0')
+	{
+	_putchar(*str);
+	s++;
+	}
+	_putchar('\n');
+}
