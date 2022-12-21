b391adf066717b8848af435738180c8c38cd9630
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-20 16:39:53 -0800

    Auto

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ **_strcpy - copying function
+ *
+ *@dest: arg
+ *@src: arg
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
+
+	while (*(src + 1) != '\0')
+	{
+	l++;
+	}
+	for ( ; x < 1; x++)
+	{
+	dest[x] = src[x];
+	}
+	dest[1] = '\0';
+	return (dest);
+}
