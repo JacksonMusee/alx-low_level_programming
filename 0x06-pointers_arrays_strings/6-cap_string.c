7e4641dd44450cd78a5391cf8fddc6bd821ea356
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-22 10:57:45 -0800

    Auto

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,9 +20,9 @@ char *cap_string(char *str)
 	str[0] = str[0] - 32;
 	}
 	if ((str[i] >= 97 && str[i] <= 122)  &&
-	((!(str[i - 1] >= 65 && str[i - 1] <= 90)) &&
+	(str[i - 1] != 45) && (!(str[i - 1] >= 65 && str[i - 1] <= 90)) &&
 	(!(str[i - 1] >= 97 && str[i - 1] <= 122)) &&
-	(!(str[i - 1] >= 48 && str[i - 1] <= 57))))
+	(!(str[i - 1] >= 48 && str[i - 1] <= 57)))
 	{
 	str[i] = str[i] - 32;
 	}
