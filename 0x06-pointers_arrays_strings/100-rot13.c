e9481c6dc9d79017e1cca6659f1940df98347f06
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-22 12:45:37 -0800

    Auto

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,27 +5,27 @@
 *
 * Return: encode string
 */
+
 char *rot13(char *str)
 {
-int i, j;
+	int i, j;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-i = 0;
-while (str[i] != '\0')
-{
-j = 0;
-while (alpha[j] != '\0')
-{
-if (str[i] == alpha[j])
-{
-str[i] = rot[j];
-break;
-}
-j++;
-}
-i++;
-}
-return (str);
+	i = 0;
+	while (str[i] != '\0')
+	{
+	j = 0;
+	while (alpha[j] != '\0')
+	{
+	if (str[i] == alpha[j])
+	{
+	str[i] = rot[j];
+	break;
+	}
+	j++;
+	}
+	i++;
+	}
+	return (str);
 }
