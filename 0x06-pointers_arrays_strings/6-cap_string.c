d27dbff52bde87f986c0444b1fa9e1adf9d2ecee
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-22 10:40:59 -0800

    Auto

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,16 +6,23 @@
  *
  *@str: string
  *
- *
+ *Return: caped string
  */
 
 char *cap_string(char *str)
 {
-	int i;
+	int i = 1;
 
 	while (str[i] != '\0')
 	{
-	if (i == 0 || (str[i] >= 97 && str[i] <= 122 && (str[i-1] == 32 || str[i-1] == 46)))
+	if (str[0] >= 97 && str[0] <= 122)
+	{
+	str[0] = str[0] - 32;
+	}
+	if ((str[i] >= 97 && str[i] <= 122)  &&
+	((!(str[i - 1] >= 65 && str[i - 1] <= 90)) &&
+	(!(str[i - 1] >= 97 && str[i - 1] <= 122)) &&
+	(!(str[i - 1] >= 48 && str[i - 1] <= 57))))
 	{
 	str[i] = str[i] - 32;
 	}
