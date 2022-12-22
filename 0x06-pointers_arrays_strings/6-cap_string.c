f2b530b0b0131bc23a5d17305f8ce0648464d020
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-22 11:41:05 -0800

    Auto

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,26 +11,30 @@
 
 char *cap_string(char *str)
 {
-	int i = 1;
+	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
-	if (str[0] >= 97 && str[0] <= 122)
-	{
-	str[0] = str[0] - 32;
-	}
-	if ((str[i] >= 97 && str[i] <= 122)  &&
-	(str[i - 1] != 45) && (!(str[i - 1] >= 65 && str[i - 1] <= 90)) &&
-	(!(str[i - 1] >= 97 && str[i - 1] <= 122)) &&
-	(!(str[i - 1] >= 48 && str[i - 1] <= 57)))
-	{
-	str[i] = str[i] - 32;
-	}
-	if (str[i] == 9)
-	{
-	str[i] = 32;
-	}
-	i++;
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		i++;
+
+		if (str[i - 1] == ' ' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
+		i == 0)
+		str[i] -= 32;
+
+		i++;
 	}
 
 	return (str);
