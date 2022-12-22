4b1279d50643da3f71fb61cedc7a4fd4c1d8590d
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-22 00:20:39 -0800

    Auto

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *reverse_array - See code
+ *
+ *@a: array
+ *@n: Size of a
+ *
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	for (i = 0; n >= 0; i++)
+	{
+	int[i] = int[n];
+	n--;
+	}
+}
