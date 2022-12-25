4a3fdd6786a3986f4cf4827fce4ea58ad930269e
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-25 11:55:12 -0800

    Do times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/*
+ *times_table - See code
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int product;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; i < 10; j++)
+		{
+		product = i + j;
+		if (product > 9)
+		{
+		_putchar(' ');
+		secDigit = product % 10;
+		firDigit = (product / 10) % 10;
+		_putchar(firDigit + '0');
+		_putchar(secDigit + '0');
+		}
+		else if (product < 10 && j != 0)
+		{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(product + '0');
+		}
+		else
+		{
+		_putchar(product + '0');
+		}
+		if (j < 9)
+		{
+		_putchar(',');
+		}
+		}
+		j++;
+	}
+}
