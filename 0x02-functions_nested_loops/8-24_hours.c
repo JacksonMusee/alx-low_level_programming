f955939ab3fed8ab66c306ea287d925fdcf0d695
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-25 09:23:06 -0800

    Print Jacks Time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,55 @@
+include "main.h"
+
+/**
+ *jack_bauer - print jack's long day
+ *
+ *
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hour;
+	int min;
+	int fDigit;
+	int secDigit;
+	int mFDigit;
+	int mSecDigit;
+
+	hour = 0;
+	min = 0;
+
+	while (hour < 24)
+	{
+	for (min = 0; min < 60; min++)
+	{
+	if (hour < 10)
+	{
+	_putchar('0');
+	_putchar(hour + '0');
+	}
+	else
+	{
+	fDigit = (hour/10) % 10;
+	secDigit = hour % 10;
+	_putchar(fDigit + '0');
+	_putchar(secDigit + '0');
+	}
+	_putchar(':');
+	if (min < 10)
+	{
+	_putchar('0');
+	_putchar(min + '0');
+	}
+	else
+	{
+	mFDigit = (min/10) % 10;
+	mSecDigit = min % 10;
+	_putchar(mFDigit  + '0');
+	_putchar(mSecDigit + '0');
+	}
+	}
+	}
+
+
+}
