2f87e8a132055e4ed01e681431cb0ceb29c0dba7
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-26 01:59:44 -0800

    Prnt untll 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,20 +8,13 @@ void print_to_98(int n)
 	int cn = n;
 	int i;
 	int lstDgt;
-	int nbrHolder;
 
 	if  (cn  <= 98)
 	{
 		for (i = 0; i <= 98 - n; i++)
 		{
 		cn += i;
-		nbrHolder = cn;
-			while (nbrHolder != 0)
-			{
-			lstDgt = nbrHolder % 10;
-			_putchar(48 + lstDgt);
-			nbrHolder = nbrHolder / 10;
-			}
+		print(cn);
 		}
 		if (cn < 98)
 		{
@@ -33,17 +26,24 @@ void print_to_98(int n)
 		for (i = 0; i < n - 98; i++)
 		{
 		cn -= i;
-		nbrHolder = cn;
-			while (nbrHolder != 0)
-			{
-			lstDgt = nbrHolder % 10;
-			_putchar(48 + lstDgt);
-			nbrHolder = nbrHolder / 10;
-			}
+		print(cn);
 		}
 		if (cn > 98)
 		{
 		_putchar(',');
 		}
 	}
+}
+
+
+void print(int n)
+{
+	if (n < 0)
+	{
+	_putchar('-');
+	n = -n;
+	}
+	if (n / 10)
+	print(n / 10);
+	_putchar('0' + n % 10);
 }
