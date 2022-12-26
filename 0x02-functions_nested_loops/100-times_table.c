88f4b0c2957893d7478384853310677321f10e7b
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-26 03:50:15 -0800

    Do 'n' times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *print_times_table - print times
+ *
+ *@n - maximum number
+ */
+void print_times_table(int n);
+{
+	int i;
+	int j;
+	int product;
+
+	if (n > 15 || n < 0)
+	return;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; i++)
+		{
+		product = i * j;
+		if (product < 10 && j > 0)
+		{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		}
+		if (product >= 10 && product < 100 && j > 0)
+		{
+		_putchar(' ');
+		_putchar(' ');
+		}
+		if (product >= 100 && product < 1000 && j > 0)
+		{
+		_putchar(' ');
+		}
+		print(product);
+		_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
