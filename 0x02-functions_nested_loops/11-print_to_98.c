2f32ebfc9c420574011b5d1e6d909850476bb60a
JacksonMusee <58480110+JacksonMusee@users.noreply.github.com>
2022-12-26 13:48:13 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,55 +1,55 @@
 #include "main.h"
 /**
- *print_to_98 - See code
- *@n: First number
- *
- * print - help print numbers
- */
+*print_to_98 - See code
+*@n: First number
+*
+* print - help print numbers
+*/
 void print_to_98(int n)
 {
-	int cn = n;
-	int i;
+int cn = n;
+int i;
 
-	if  (cn  <= 98)
-	{
-		for (i = 0; i <= (98 - n); i++)
-		{
-		cn += i;
-		print(cn);
-		if (cn < 98)
-		{
-		 _putchar(',');
-		 _putchar(' ');
-		}
-		cn = n;
-		}
-	}
-	else
-	{
-		for (i = 0; i <= (n - 98); i++)
-		{
-		cn -= i;
-		print(cn);
-		if (cn > 98)
-		{
-		_putchar(',');
-		_putchar(' ');
-		}
-		cn = n;
-		}
-	}
-	_putchar('\n');
+if  (cn  <= 98)
+{
+for (i = 0; i <= (98 - n); i++)
+{
+cn += i;
+print(cn);
+if (cn < 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+cn = n;
+}
+}
+else
+{
+for (i = 0; i <= (n - 98); i++)
+{
+cn -= i;
+print(cn);
+if (cn > 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+cn = n;
+}
+}
+_putchar('\n');
 }
 
 
 void print(int n)
 {
-	if (n < 0)
-	{
-	_putchar('-');
-	n = -n;
-	}
-	if (n / 10)
-	print(n / 10);
-	_putchar('0' + n % 10);
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n / 10)
+print(n / 10);
+_putchar('0' + n % 10);
 }
