cfe7b598e491fb56960ea2557abc21830b06e530
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-26 05:55:23 -0800

    Print Fibonacci Sequence

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - Prnt Fibonacci sequence
+ *
+ *Return: zero
+ *
+ */
+int main(void)
+{
+	int x = 1;
+	int y = 2;
+	int z;
+	int count = 2;
+
+	printf("%d, %d", x,y);
+	while (count < 50)
+	{
+	z = x + y;
+	printf("%d", z);
+	if (count < 49)
+	{
+	printf(", ");
+	}
+	x = y;
+	y = z;
+	count++;
+	}
+return (0);
+}
