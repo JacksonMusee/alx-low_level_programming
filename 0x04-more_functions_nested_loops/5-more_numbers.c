4e18c4016b5167cf047cb65a887e7baf47716ce7
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-29 10:42:10 -0800

    Auto

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,14 +10,19 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int base;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
+		if (i >= 10)
+		{
+		base = i / 10;
+		}
+		_putchar(48 + base);
 		_putchar(48 + j);
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
