306583050d39afa7d3bfd6041b44207105a0e76e
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-30 09:24:00 -0800

    Auto

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *print_diagonal - See code
+ *@n: Times to print
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; i++)
+		{
+		_putchar(' ');
+		}
+		_putchar('\\');
+	}
+	_putchar('\n');
+	}
+	else
+	{
+	_putchar('\n');
+	}	
+}
