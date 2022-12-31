8b89fcf1914da8cc6bed80cbec900cd377b78535
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-31 05:10:03 -0800

    Auto

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,21 +2,21 @@
 
 /**
  *print_number - do it
- *
+ *@n: number to print
  */
 
 void print_number(int n)
 {
 	if (n < 0)
 	{
-	n = -n
+	n = -n;
 	_putchar('-');
 	}
 
 	if (n / 10)
 	{
-	print_numbers(n/10);
+	print_number(n / 10);
 	}
 
-	_putchar(n % 10);
+	_putchar(48 + (n % 10));
 }
