f1fe1388f52b63166e2483b3b0c51b4451151e9d
JacksonMusee <jacksonmutisya94@gmail.com>
2022-12-31 00:17:25 -0800

    Auto

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,23 @@ void fizz_buzz(void)
 	{
 	if ((i % 3 == 0) && (i % 5 == 0))
 	{
-	printf("FizzBuzz ");
+	printf("FizzBuzz");
 	}
 	else if (i % 3 == 0)
 	{
-	printf("Fizz ");
+	printf("Fizz");
 	}
 	else if (i % 5 == 0)
 	{
-	printf("Buzz ");
+	printf("Buzz");
 	}
 	else
 	{
-	printf("%d ", i);
+	printf("%d", i);
+	}
+	if (i < 100)
+	{
+	printf(" ");
 	}
 	}
 	printf("\n");
@@ -34,6 +38,7 @@ void fizz_buzz(void)
 /**
  *main - entry
  *
+ *Return: 1 for success
  */
 
 int main(void)
