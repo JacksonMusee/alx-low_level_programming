fd7914355c3c21ffd562019c6eb92134807fc49b
JacksonMusee <jacksonmutisya94@gmail.com>
2023-01-07 05:02:08 -0800

    Auto

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strstr - Get first occurrennce of a substring
+ *@haystack: Man string
+ *@needle: Substring
+ *
+ *Return: Pointer to first occurrence of sub string
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *first_occ = NULL;
+	char *needle_origi;
+
+	while (*haystack != '\0')
+	{
+		while (*haystack != *needle)
+		{
+		haystack++;
+		}
+		if (*hastack == *needle)
+		{
+		first_occ = haystack;
+
+		while (*hastack == *needle && *haystack != '\0' && *needle != '\0')
+		{
+		haystack++;
+		needle++;
+		}
+		if (*needle == '\0')
+		{
+		break;
+		}
+		else
+		{
+		haystack = first_occ;
+		needle = needle_origi;
+		}
+		}
+		haystack++;
+	}
+	return (first_occ);
+}
