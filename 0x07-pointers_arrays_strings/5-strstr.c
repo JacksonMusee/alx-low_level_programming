f54d1d00a62215a3f90bd425fd9abf131ae855c6
JacksonMusee <jacksonmutisya94@gmail.com>
2023-01-07 05:35:59 -0800

    Auto

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	char *first_occ = NULL;
 	char *needle_origi = needle;
 
-	if (*needle[0] == '\0')
+	if (needle[0] == '\0')
 	{
 	return (haystack);
 	}
