bc07f63c65844e24d8befea097b0b8cc1039fb52
JacksonMusee <jacksonmutisya94@gmail.com>
2023-01-07 06:56:45 -0800

    Auto

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -22,7 +22,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 	while (*haystack != '\0')
 	{
-		while (*haystack != *needle)
+		while (*haystack != *needle && *haystack != '\0')
 		{
 		haystack++;
 		}
