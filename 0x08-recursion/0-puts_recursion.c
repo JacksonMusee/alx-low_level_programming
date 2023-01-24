428f2b00eaa5c112041c899b25411ee4ad94092b
JacksonMusee <jacksonmutisya94@gmail.com>
2023-01-24 12:28:51 -0800

    Puts using recurson

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  *_puts_recursion - Print string
  *
  *@s: Pointer to a string
@@ -8,12 +8,14 @@
  */
 
 void _puts_recursion(char *s)
-{	
+{
 	int c = *s;
 
 	if (c == '\0')
+	{
+	_putchar('\n');
 	return;
-
+	}
 	_putchar(c);
 	s++;
 	_puts_recursion(s);
