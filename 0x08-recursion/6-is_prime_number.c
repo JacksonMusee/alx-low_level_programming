98104eb2337eaae39d085873c876015180bb6097
JacksonMusee <jacksonmutisya94@gmail.com>
2023-01-29 11:12:55 -0800

    Check if a number is prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *is_prime_number - check if a number is prime
+ *
+ *@n: the number
+ *
+ *Return: 1 if prime 0 if not prime
+ */
+
+int is_prime_number(int n)
+{
+	const int num = n;
+
+	n--;
+	if (num % n == 0 || num < 2)
+	{
+	return (0);
+	}
+
+	if (n > 1)
+	{
+	is_prime_number (n);
+	}
+
+	return (1);
+}
