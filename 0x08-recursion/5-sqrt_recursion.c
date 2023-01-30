9b28b313a49fac11d0f2c93a1a10287569bc86ed
JacksonMusee <jacksonmutisya94@gmail.com>
2023-01-30 11:05:00 -0800

    Find squareroot of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,7 +9,7 @@
  *Return: Square root or -1 if the number has no natural sqrt
  *
  */
-int _sqrt_recurson_helper(int n, int i)
+int _sqrt_recursion_helper(int n, int i)
 {
 	if (i * i == n)
 	return (i);
@@ -17,7 +17,7 @@ int _sqrt_recurson_helper(int n, int i)
 	if (i * i > n)
 	return (-1);
 
-	return (_sqrt_recurson_helper(n, i++));
+	return (_sqrt_recursion_helper(n, i++));
 }
 
 /**
@@ -33,5 +33,5 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 	return (-1);
 
-	return (_sqrt_recurson_helper(n, 0));
+	return (_sqrt_recursion_helper(n, 1));
 }
