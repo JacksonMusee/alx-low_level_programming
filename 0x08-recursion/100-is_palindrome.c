071f6f2a2ef59097ad54e0024357e33edac0a7f1
JacksonMusee <jacksonmutisya94@gmail.com>
2023-02-01 13:14:48 -0800

    Check if a string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -32,7 +32,7 @@ int is_palindrome_helper(char *x, char *y)
 
 	if (*x == *y)
 	{
-	rs = 1;
+	rs = 2;
 	}
 
 	x = x + 1;
@@ -53,7 +53,7 @@ int is_palindrome(char *s)
 	int r;
 
 	if (*s == '\0')
-	return (2);
+	return (1);
 
 	end = go_to_last(s);
 	r = is_palindrome_helper(s, end);
