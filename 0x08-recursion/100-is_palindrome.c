f4c2cd4661d3996048da89493653549dac848c0c
JacksonMusee <jacksonmutisya94@gmail.com>
2023-02-01 12:10:39 -0800

    Check if a string is a palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+char *go_to_last(char *s)
+{
+	int endval = *s;
+
+	if (end != '\0')
+	{
+	s++;
+	go_to_last(s);
+	}
+
+	return (s);
+}
+
+
+int is_palindrome_helper(char *x, char *y)
+{
+	if (*x == *y)
+	return (1);
+
+	if (*x != *y)
+	return (0);
+
+	if (*x == '\0')
+	return (1);
+
+	x++;
+	y++;
+
+	return (is_palindrome_helper(x, y));
+}
+
+
+int is_palindrome(char *s)
+{
+	char *end;
+	int r;
+
+	if (*s == '\0')
+	return (1);
+
+	end = go_to_last(s);
+	r = is_palindrome_helper(s, end);
+
+	return(r);
+}
