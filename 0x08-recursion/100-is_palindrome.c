03ce21b8b71332ca880ca188b4bc2e2fd27d8464
JacksonMusee <jacksonmutisya94@gmail.com>
2023-02-02 12:38:07 -0800

    Check if a string is palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
+ *go_to_last - move pointer to last element of the string
  *
+ *@s: The string
  *
- *
- *
- *
+ *Return: returns pointer to the last character of the string
  */
 
 char *go_to_last(char *s)
@@ -21,6 +21,15 @@ char *go_to_last(char *s)
 	return (go_to_last(s));
 }
 
+/**
+ *is_palindrome_helper - loop through the string comparing characters in pairs
+ *
+ *@x: Pointer to first character of the string
+ *
+ *@y: pointer to the last character of the string
+ *
+ * Return: 0 if there character pair not matching and 1 if all matched
+ */
 
 int is_palindrome_helper(char *x, char *y)
 {
@@ -40,6 +49,13 @@ int is_palindrome_helper(char *x, char *y)
 	return (is_palindrome_helper(x, y));
 }
 
+/**
+ *is_palindrome - take a string and check if it's a palindrome
+ *
+ *@s: The string
+ *
+ *Return: 1 if its a palindrome and 0 otherwise.
+ */
 
 int is_palindrome(char *s)
 {
@@ -52,5 +68,5 @@ int is_palindrome(char *s)
 	end = go_to_last(s);
 	r = is_palindrome_helper(s, end);
 
-	return(r);
+	return (r);
 }
