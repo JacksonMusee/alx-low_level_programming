23154f1a524b7377541bf758db176450a658c56a
JacksonMusee <jacksonmutisya94@gmail.com>
2023-02-05 07:51:23 -0800

    Compare two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -34,15 +34,15 @@ int wildcmp(char *s1, char *s2)
 	return (0);
 	}
 
-	if (*s1 == '\0' && *s2 == '\0')
-	{
-	return (1);
-	}
-
 	if (*s1 == *s2)
 	{
 	s1++;
 	s2++;
+
+		if (*s1 == '\0' && *s2 == '\0')
+		{
+		 return (1);
+		}
 	}
 
 	return (wildcmp(s1, s2));
