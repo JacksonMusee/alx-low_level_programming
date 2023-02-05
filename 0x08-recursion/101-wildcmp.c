c2d59a530340cf4e6bf3d27a87930e6dcadc0f00
JacksonMusee <jacksonmutisya94@gmail.com>
2023-02-05 06:51:18 -0800

    Compare two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,12 +17,12 @@ int wildcmp(char *s1, char *s2)
 	wildcmp(s1, s2);
 	}
 
-	if (*s1 != *s2 && *s2 != 42) 
+	if (*s1 != *s2 && *s2 != 42)
 	{
 	s1++;
 	wildcmp(s1, s2);
 	}
-	
+
 	if (*s1 == '\0' && *s2 == 42)
 	{
 	s2++;
