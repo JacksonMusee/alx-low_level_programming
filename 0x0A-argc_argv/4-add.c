cb820ae6331cf913e0db98239a47b425d0f12dfa
JacksonMusee <jacksonmutisya94@gmail.com>
2023-02-11 10:31:18 -0800

    Add all numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *main - see code
+ *
+ *@argc: argument count
+ *@argv: Argument vector
+ *
+ * Return: 0 success 1 error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+	int num;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (isdigit(*argv[0]) == 0)
+		{
+		printf("Error\n");
+		return (1);
+		}
+
+		num = atoi(*argv);
+		sum += num;
+		argv++;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
