2505a109661c81cf90c6940dab721c9889dc26a3
JacksonMusee <jacksonmutisya94@gmail.com>
2023-02-19 11:39:08 -0800

    Create an array an allocate memory usng malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,13 +21,15 @@ char *create_array(unsigned int size, char c)
 	return (NULL);
 	}
 
-	str = malloc((sizeof(char) * size) + 1);
+	str = malloc(sizeof(char) * size);
+
+	if (str == NULL)
+	return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
 	str[i] = c;
 	}
-	str[i] = '\0';
 
 	return (str);
 }
