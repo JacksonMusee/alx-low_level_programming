e5516eae2536bfc7512d0e3072713b27e80edc8d
JacksonMusee <jacksonmutisya94@gmail.com>
2023-02-22 09:02:36 -0800

    Create 2-D array using malloc

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlb.h>
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < heght; i++)
+	{
+	free(grid[i]);
+	}
+
+	free(grid);
+}
