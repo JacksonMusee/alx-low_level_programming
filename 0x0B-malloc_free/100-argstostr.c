81da0754a2f890ce0f7111873d00f1b1e70e71ad
JacksonMusee <jacksonmutisya94@gmail.com>
2023-02-22 10:17:38 -0800

    Make one string from all command line arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,76 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *char_count - count number of charater in the array av
+ *
+ *@ac: aurguments count equivalent to number of elements in av
+ *@av: array of argments
+ *
+ * Return: number of charaters
+ */
+
+int char_count(int ac, char **av)
+{
+	int count = 0;
+	int i, j;
+
+	for (i = 1; i < ac; i++)
+	{
+	j = 0;
+
+	while (av[i][j] != '\0')
+	{
+	count++;
+	j++;
+	}
+	}
+
+	return (count);
+}
+
+/**
+ *argstostr - concatenate all arguments to a string
+ *
+ *@ac: argument count
+ *@av: array of all arguemnts
+ *
+ *Return: pointer to the new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int c_count;
+	int i, j;
+	int k = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	c_count = char_count(ac, av);
+
+	str = malloc(sizeof(char) * (c_count + ac - 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 1; i < ac; i++)
+	{
+		j = 0;
+
+		while (av[i][j] != '\0')
+		{
+		str[k] = av[i][j];
+		k++;
+		j++;
+		}
+		if (i == ac - 1)
+		str[k] = '\0';
+
+		str[k] = '\n';
+	}
+
+	return (str);
+}
