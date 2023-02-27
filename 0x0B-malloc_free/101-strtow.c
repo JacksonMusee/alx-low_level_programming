3f3ea73857a3826d91c8b78a5bd8daf5ed2b0dd8
JacksonMusee <jacksonmutisya94@gmail.com>
2023-02-27 12:34:35 -0800

    Wait

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,102 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *do_count - Count words and characters in a string
+ *
+ *@str: the string
+ *
+ *Return: A pointer to array of character count and word count
+ */
+
+int *do_count(char *str)
+{
+	int c_count;
+	int w_count;
+	int count[2];
+
+	while (*str == ' ' || *str == '\t')
+	{
+	str++;
+	}
+
+	while (*str != '\0')
+	{
+		if (*str == ' ' || *str == '\t')
+			str++;
+
+		c_count++;
+
+		if ((*str != ' ' && *str != '\t') && (*(str - 1) == ' ' || *(str - 1) == '\t'))
+			w_count++;
+			c_count++;
+
+		str++;
+	}
+
+	count[0] = c_count;
+	count[1] = w_count;
+
+	return (count);
+}
+
+/**
+ *strtow - Convert a string to an array of string (Words)
+ *
+ *@str: The string
+ *
+ * Return: pointer to the array
+ */
+
+char **strtow(char *str)
+{
+	char **words;
+	int i;
+	int j = 0;
+	int c_count = 0;
+	int w_count = 0;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	words = malloc((sizeof(char) * (c_count + 1)) + (sizeof(char) * (w_count + 1)));
+
+	if (words == NULL)
+		return (NULL);
+
+	for (i = 0; i < w_count; i++)
+	{
+		if (*str == ' ' || *str == '\t')
+			str++;
+
+		while (*str != ' ' && *str != '\t' && *str != '\0')
+		{
+			words[i][j] = *str;
+			str++;
+			j++;
+		}
+
+		words[i][j] = '\0';
+		j = 0;
+	}
+
+	return (words);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
