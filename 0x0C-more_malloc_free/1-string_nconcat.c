3e1ece05dbe8b40bd6fd79d5c40986c2442081e9
JacksonMusee <jacksonmutisya94@gmail.com>
2023-03-05 06:13:42 -0800

    Concatenate strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,43 +3,67 @@
 #include <stdlib.h>
 
 /**
+ * str_size - Find lengthe of a string
+ *
+ * @strg: Thr string
+ * 
+ *Return: The length
+ *
+ */
+
+int str_size(char *strg)
+{
+	int len = 0;
+
+	while (*strg != '\0')
+	{
+	len++;
+	strg++;
+	}
+
+	return (len);
+
+}
+/**
  *string_nconcat - Concatenate string with a fraction of another string
  *
  *@s1: String one
  *@s2: String two
  *@n: Chacters of s2 to be used
  *
- * Return: Ponter to teh new string
+ *Return: Pointer to teh new string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
+	int j = 0;
 	char *new_str;
+	int s1_size;
 
-	new_str = malloc(sizeof(s1) + n + 1);
+	s1_size = str_size(s1);
+
+	new_str = malloc(sizeof *new_str * (s1_size + n + 1));
 
 	if (new_str == NULL)
 		return (NULL);
 
 	while (*s1 != '\0')
 	{
-		*new_str = *s1;
-
-		new_str++;
+		new_str[j] = *s1;
+		j++;
 		s1++;
 	}
 
 	while (i < n)
 	{
-		*new_str = *s2;
-
-		new_str++;
+		new_str[j] = *s2;
 		s2++;
 		i++;
+		j++;
 	}
 
-	*new_str = '\0';
+	new_str[j] = '\0';
 
 	return (new_str);
 }
