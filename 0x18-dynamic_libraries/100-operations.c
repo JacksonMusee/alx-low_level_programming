85ab22bce70e0936764e213beb8f47fde40b8b87
JacksonMusee <jacksonmutisya94@gmail.com>
2023-03-25 04:35:08 -0700

    Revisit C

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+ * mod - see code
+ * @a: number
+ * @b: number
+ * Return: see code
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * mod - see code
+ * @a: number
+ * @b: number
+ * Return: see code
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mod - see code
+ * @a: number
+ * @b: number
+ * Return: see code
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * mod - see code
+ * @a: number
+ * @b: number
+ * Return: see code
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - see code
+ * @a: number
+ * @b: number
+ * Return: see code
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
