e2ae083b53ff4447acc2e948c4ead75542fda46d
JacksonMusee <jacksonmutisya94@gmail.com>
2023-04-23 10:15:47 -0700

    Push

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *init_dog - First function
+ *
+ *@d: Name of struct
+ *@name: Dog name
+ *@age: Miaka
+ *@owner: Mwenyewe
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
