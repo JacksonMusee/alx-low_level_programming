829c6511fa229cf625b97f55d1c09fa8d4af8b16
JacksonMusee <jacksonmutisya94@gmail.com>
2023-04-28 11:55:38 -0700

    Push

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,33 +4,33 @@
 
 void print_dog(struct dog *d)
 {
-	if(d)
-	{
-		if(d->name)
-		{
-		printf("Name: %s\n", d->name);
-		}
-		else
-		{
-		printf("Name: (nil)\n");
-		}
+if (d)
+{
+if (d->name)
+{
+printf("Name: %s\n", d->name);
+}
+else
+{
+printf("Name: (nil)\n");
+}
 
-		if(d->age)
-		{
-                printf("Age: %f\n", d->age);
-                }
-                else
-                {
-                printf("Age: 0.000000\n");
-                }
+if (d->age)
+{
+printf("Age: %f\n", d->age);
+}
+else
+{
+printf("Age: 0.000000\n");
+}
 
-		if(d->owner)
-                {
-                printf("Owner: %s\n", d->owner);
-                }
-                else
-                {
-                printf("Owner: (nil)\n");
-                }
-	}
+if (d->owner)
+{
+printf("Owner: %s\n", d->owner);
+}
+else
+{
+printf("Owner: (nil)\n");
+}
+}
 }
