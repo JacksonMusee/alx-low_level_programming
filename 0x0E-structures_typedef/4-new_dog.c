64f6cfa66c1eaa686109ed3e366d27ef0f7746db
JacksonMusee <jacksonmutisya94@gmail.com>
2023-04-28 23:42:04 -0700

    Push

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *new_dog - Create a new dog
+ *
+ *@name: name of your dog
+ *@age: age of the dog
+ *@owner: Who owns you
+ *
+ * Return: A ponter to the dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	kadog = malloc(sizeof(dog_t));
+	kadog.name = name;
+	kadog.age = age;
+	kadog.owner = owner;
+
+	return kadog;
+}
