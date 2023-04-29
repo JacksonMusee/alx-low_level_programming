c3a8f24cbd74cb0f14b4f20d747679cb4d9b31a9
JacksonMusee <jacksonmutisya94@gmail.com>
2023-04-29 04:35:19 -0700

    Push

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 
 /**
@@ -17,6 +18,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *kadog;
 
 	kadog = (dog_t *) malloc(sizeof(dog_t));
+	if (kadog == NULL)
+		return NULL;
+
+	kadog->name = (char *) malloc((sizeof(char) * strlen(name)) + sizeof(char));
+	if (kadog->name == NULL)
+	{
+	free(kadog);
+	return (NULL);
+	}
+	
+	kadog->owner = (char *) malloc((sizeof(char) * strlen(owner)) + sizeof(char));
+	if (kadog->owner == NULL)
+	{
+	free(kadog->name);
+	free(kadog);
+	return (NULL);
+	}
 
 	kadog->name = name;
 	kadog->age = age;
