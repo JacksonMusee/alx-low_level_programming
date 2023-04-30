93b60cdcf08e68335009ec6f9a7dbcd6b992d34b
JacksonMusee <jacksonmutisya94@gmail.com>
2023-04-30 06:22:56 -0700

    Push

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ *print_name - Print a name using a function far away
+ *
+ *@name: The aneme to print
+ *@f: The function from far east!
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+I'm never empty even when I got nothing!
