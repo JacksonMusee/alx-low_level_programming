e843884aabb2f9f26285dfa52af3db5e8affd9f5
JacksonMusee <jacksonmutisya94@gmail.com>
2023-05-01 12:57:11 -0700

    Push

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -4,58 +4,58 @@
 void print_bytes(int bytes);
 
 /**
- *main - Entry to complication
- *
- *@argc: Argument count
- *@argv: Argument vector
- *
- *Return: 0 on success
- *
- */
+*main - Entry to complication
+*
+*@argc: Argument count
+*@argv: Argument vector
+*
+*Return: 0 on success
+*
+*/
 int main(int argc, char **argv)
 {
-	int bytes;
+int bytes;
 
-	if (argc != 2)
-	{
-	printf("Error\n");
-	exit(1);
-	}
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
 
-	bytes = atoi(argv[1]);
+bytes = atoi(argv[1]);
 
-	if (bytes < 0)
-	{
-	printf("Error\n");
-	exit(2);
-	}
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
 
-	print_bytes(bytes);
+print_bytes(bytes);
 
-	return (0);
+return (0);
 }
 
 /**
- *print_bytes - the first bytes of main
- *
- *@bytes: The number to print
- *
- */
+*print_bytes - the first bytes of main
+*
+*@bytes: The number to print
+*
+*/
 void print_bytes(int bytes)
 {
-        int i = 0;
-        unsigned char *main_n;
-        main_n = (char *)main;
+int i = 0;
+unsigned char *main_n;
+main_n = (char *)main;
 
-        while (i < bytes)
-        {
+while (i < bytes)
+{
 
-		if (i != 0)
-		{
-		printf(" ");
-		}
-        	printf("%2x", main_n[i]);
-		i++;
-        }
-        printf("\n");
+if (i != 0)
+{
+printf(" ");
+}
+printf("%2x", main_n[i]);
+i++;
+}
+printf("\n");
 }
