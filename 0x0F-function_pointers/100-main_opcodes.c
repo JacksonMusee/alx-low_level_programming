f073b8dd3a50fe0530f4affc1d556667499d5e37
JacksonMusee <jacksonmutisya94@gmail.com>
2023-05-01 12:04:03 -0700

    Push

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]);
+
+/**
+ *print_bytes - the first bytes of main
+ *
+ *@bytes: The number to print
+ *
+ */
+void print_bytes(int bytes)
+{
+	int i;
+	unsigned char *main_n;
+	main_n = (unsigned char *)&main;
+
+	for (i = 0; i < bytes; i++)
+	{
+	printf("%2x ", *(main_n + i));
+	}
+	printf("%2x\n", *(main_n + bytes));
+}
+
+/**
+ *main - Entry to complication
+ *
+ *@argc: Argument count
+ *@argv: Argument vector
+ *
+ *Return: 0 on success
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int bytes;
+
+	if (argc != 2)
+	{
+	printf("Error");
+	exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+	printf("Error");
+	exit(2);
+	}
+
+	print_bytes(bytes);
+}
