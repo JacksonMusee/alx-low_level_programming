634b4c4d5bd97b4679a17bc167e55d55d267f56c
JacksonMusee <58480110+JacksonMusee@users.noreply.github.com>
2023-10-02 21:16:08 +0300

    _

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *calc_len - find number of characters in a string
+ *
+ *@str_cp: the string
+ *
+ * Return: returns the length
+ */
+
+int calc_len(char *str_cp)
+{
+	int len = 0;
+
+	if (str_cp == NULL)
+	return (0);
+
+	while (*str_cp != '\0')
+	{
+	len += 1;
+	str_cp++;
+	}
+
+	return (len);
+}
+
+/**
+ *str_concat - concatenate two strngs
+ *
+ *@s1: String 1
+ *@s2: string 2
+ *
+ *
+ * Return: pointer to the concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *str;
+	int len1, len2;
+	int i = 0, j = 0;
+
+	len1 = calc_len(s1);
+	len2 = calc_len(s2);
+
+	str = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (str == NULL)
+	return (NULL);
+
+	while (i < len1)
+	{
+	str[i] = *(s1 + i);
+	i++;
+	}
+
+	while (j < len2)
+	{
+	str[i] = *(s2 + j);
+	j++;
+	i++;
+	}
+
+	str[i] = '\0';
+
+	return (str);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *alloc_grid - Allocate memory for 2-D array (matrix)
+ *
+ *@width: number of columns
+ *@height: number of rows
+ *
+ * Return: pointer to the matrix
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+	return (NULL);
+
+	grid = (int **)malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+	{
+	free(grid);
+	return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+	grid[i] = (int *)malloc(sizeof(int) * width);
+
+		if (grid[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+			{
+			free(grid[i]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
