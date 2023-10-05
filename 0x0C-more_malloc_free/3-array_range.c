ff328f839b45401501bb98ab1889eca280ddd005
JacksonMusee <jacksonmutisya94@gmail.com>
2023-10-05 13:42:51 -0700

    Just do it

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *array_range - arrange integers
+ *
+ *@min: Lower boarder
+ *@max: Upper limit
+ *
+ *Return: Pointer to the arranged array
+ */
+
+int *array_range(int min, int max)
+{
+	int *arranged;
+	int i = 0;
+
+	if (min > max)
+		return NULL;
+
+	arranged = malloc(sizeof(arranged)*((max - min)+1));
+
+	if (arranged == NULL)
+		return NULL;
+
+	while (i < (max - min + 1))
+	{
+		arranged[i] = min + i;
+		i++;
+	}
+	
+	return arranged;
+}
+
