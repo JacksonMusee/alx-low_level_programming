0a49b79bdca169d5b969bd8f3344c9333e01b9f9
JacksonMusee <jacksonmutisya94@gmail.com>
2023-10-12 13:51:30 -0700

    Yes

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(my_args, n);
 	for (i = 0; i < n; i++)
 	{
-		if (i == n-1)
+		if (i == n - 1)
 		{
 			printf("%d\n", va_arg(my_args, int));
 		}
@@ -28,7 +28,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		{
 			printf("%d", va_arg(my_args, int));
 
-			if (separator && i != n-1)
+			if (separator)
 				printf("%s", separator);
 
 		}
