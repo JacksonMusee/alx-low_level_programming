0c09db3d92a45be007483e2fdae804f3f2aea60d
JacksonMusee <jacksonmutisya94@gmail.com>
2023-10-12 13:04:12 -0700

    Push it

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ *sum_them_all - funscton to sum all its aurguments
+ *
+ *@n: number arguments
+ *
+ * Return - retunr the sum
+ *
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list my_args;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(my_args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(my_args, unsigned int);
+	}
+	va_end(my_args);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Variadic functions is also fun
