44e648f2364cfde8ea6b8b55c170fe77871224b9
JacksonMusee <jacksonmutisya94@gmail.com>
2023-10-20 05:02:13 -0700

    Singly 001

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *print_list - Print elements of a singly linked list
+ *
+ *@h: Pointer to the list
+ *
+ *Return: number of node printed
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+	const list_t *actv_node = h;
+
+	if (actv_node == NULL)
+		return (0);
+
+	while (actv_node != NULL)
+	{
+		if (actv_node->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] ", actv_node->len);
+			printf("%s\n", actv_node->str);
+		}
+		actv_node = actv_node->next;
+		node_count++;	
+	}
+
+	return  (node_count);
+}
