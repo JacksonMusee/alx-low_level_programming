7623019ddcd7e87a49c552e5af6e17ae52cd47de
JacksonMusee <jacksonmutisya94@gmail.com>
2023-10-20 11:38:30 -0700

    Singly 004

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_before_main(void)__attribute__((constructor));
+
+/**
+ *print_before_main - prnt somethng before main s executed
+ *
+ *
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
+}
