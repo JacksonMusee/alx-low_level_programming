d9524f00380845431304ebfac6e40484a9999d53
JacksonMusee <jacksonmutisya94@gmail.com>
2023-10-20 08:27:54 -0700

    Singly 002

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ *add_node - Add a node 
+ *
+ *@head: Pointer to pointer to the list
+ *@str: String to he new node
+ *
+ *Return: new head
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *my_str = strdup(str);
+
+	list_t *new_head = malloc(sizeof(list_t));
+
+	if (new_head == NULL)
+		return NULL;
+
+	new_head->next = (*head);
+	new_head->str = my_str;
+	new_head->len = strlen(my_str);
+
+	*head = new_head;
+
+	return (new_head);
+}
