033a380b5807b78ca7ce4c7fa58ce2cbd7a77486
JacksonMusee <jacksonmutisya94@gmail.com>
2023-10-24 01:44:47 -0700

    More sigly x005

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - Free list and set head to NULL
+ *
+ *@head: Double pointer to the list
+ *
+ *Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *curr_head = *head;
+	listint_t *next_node;
+
+	while (curr_head != NULL)
+	{
+		next_node = curr_head->next;
+		free(curr_head);
+		curr_head = next_node;
+	}
+
+	*head = curr_head;
+}
