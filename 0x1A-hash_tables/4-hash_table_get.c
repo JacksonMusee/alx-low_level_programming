35a57c310d329cc2cb83a757b248714df8ddb4bc
JacksonMusee <jacksonmutisya94@gmail.com>
2023-12-22 10:17:07 -0800

    Push

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ *hash_node_t - retrieves a value associated with a key
+ *
+ *@ht: Pointer to hash table
+ *@key: The key
+ *
+ * Return: The value associated with the element,
+ or NULL if key couldn’t be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *head;
+	char *the_value = NULL;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[index];
+
+	if (head == NULL)
+		return (NULL);
+
+	while (head)
+	{
+		if (strcmp(head->key, key) == 0)
+		{
+			the_value = head->value;
+			break;
+		}
+
+		head = head->next;
+	}
+
+	return (the_value);
+
+}
